38b3288c2ad7bea9f74d99d41129a1b72cceaa76
EMigotto <30959756+EMigotto@users.noreply.github.com>
2026-05-06 13:41:32 -0300

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import PodcastStudio from "./PodcastStudio.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <PodcastStudio />
+  </React.StrictMode>
+);
