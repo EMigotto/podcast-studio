6e4c8a89887f2e1fb5251e3a6da5767764b62385
EMigotto <30959756+EMigotto@users.noreply.github.com>
2026-05-06 21:34:48 -0300

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import PodcastStudio from "./PodcastStudio.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <PodcastStudio />
-  </React.StrictMode>
-);
